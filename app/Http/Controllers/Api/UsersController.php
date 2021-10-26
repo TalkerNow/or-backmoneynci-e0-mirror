da9f41ad4aa02ec445134fcf91f0fe26ec430a05
enzo <enzo.nivert@epitech.eu>
2021-10-26 11:59:30 +0200

    member added to request

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -60,7 +60,7 @@ class UsersController extends Controller
                 }
             }
         }
-        if ($request->kind == 'client'){
+        if ($request->kind == 'client' || $request->kind == 'member'){
         return $users->toJson(JSON_PRETTY_PRINT);
         }else if ($request->kind == 'oldclient'){
             return $oldclient->toJson(JSON_PRETTY_PRINT);
