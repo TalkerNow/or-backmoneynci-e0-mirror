b620a9d127a4572a6efac6adce46a65969929d2c
enzo <enzo.nivert@epitech.eu>
2021-11-02 14:29:59 +0100

    update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -24,7 +24,7 @@ class UsersController extends Controller
 //        if ($auth->role != "admin")
 //            return response()->json(['error' => 'Unauthorized'], 401);
         if ($request->kind == 'oldclient'){
-            if($auth->role == "admin"){
+            if($auth->role == "admin" || $auth->role == "Consultant"){
                 $oldclient = DB::select('select * from old_clients');
             }else{
                 $users = User::with('parent')->where(function ($query) {
@@ -34,7 +34,7 @@ class UsersController extends Controller
             } 
         }
         else if($request->kind == 'client'){
-            if($auth->role == "admin"){
+            if($auth->role == "admin" || $auth->role == "Consultant"){
                 $users = User::with('parent')->where('role','Client')->orderby('created_at','DESC')->get();
                 $infos = PersonalInformations::all();
             }else{
@@ -64,7 +64,7 @@ class UsersController extends Controller
             }
             return $users->toJson(JSON_PRETTY_PRINT);
         }else if ($request->kind == 'oldclient'){ 
-            return view([$oldclient]);
+            return view('oldClients',['oldclient'=>$oldclient]);
         }
     }
 
