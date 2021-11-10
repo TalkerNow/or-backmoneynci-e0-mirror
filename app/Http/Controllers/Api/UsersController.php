0f6d9203cce5e8bf0dd8dce1d9945fc53779be86
enzo <enzo.nivert@epitech.eu>
2021-11-10 15:08:49 +0100

    call self

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -45,7 +45,7 @@ class UsersController extends Controller
         if ($request->kind == 'oldclient'){
             if($auth->role == "admin" || $auth->role == "Consultant" || $auth->role == "Expert"){
                 $oldclients = OldClients::all();
-                $oldclients = convert_from_latin1_to_utf8_recursively($oldclients);
+                $oldclients = self::convert_from_latin1_to_utf8_recursively($oldclients);
                 return response()->json(['data' => [$oldclients]], 200); 
                 
                 return $oldclients->toJson(JSON_PRETTY_PRINT);
