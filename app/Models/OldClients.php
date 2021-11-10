eb391ba896ef9e5c5671bf985364b712ed4fc491
enzo <enzo.nivert@epitech.eu>
2021-11-10 15:47:03 +0100

    removed memo from old client modele

--- app/Models/OldClients.php
+++ app/Models/OldClients.php
@@ -20,6 +20,6 @@ class OldClients extends Model
         'cl_tel_port', 'cl_mail', 'cl_ss1', 'cl_ss2', 'cl_ne_le', 'cl_ne_a', 'cl_nb_enf', 'cl_nationalite', 'cl_sifacleunik',
         'cl_date_sit_fam', 'cl_date_retraite', 'cl_conjoint_nom', 'cl_conjoint_prenom', 'cl_conjoint_ss1', 'cl_conjoint_ss2',
         'cl_conjoint_ne_le', 'cl_conjoint_decede_le', 'cl_etude_sup', 'cl_etude_nb_annee', 'cl_tyetcleunik', 'cl_diplome_obtenu',
-        'cl_service_mil', 'cl_annee_service_mil', 'cl_duree_service_mil', 'cl_tysemicleunik', 'cl_date', 'cl_source', 'cl_memo', 'cl_assurance', 'findossier'
+        'cl_service_mil', 'cl_annee_service_mil', 'cl_duree_service_mil', 'cl_tysemicleunik', 'cl_date', 'cl_source', 'cl_assurance', 'findossier'
     ];
 }
