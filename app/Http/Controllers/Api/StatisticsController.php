e80dca6245cddfd28f07187846f34b91610f7f36
enzo <enzo.nivert@epitech.eu>
2021-12-22 10:35:38 +0100

    test

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -176,6 +176,7 @@ class StatisticsController extends Controller
                 ->whereYear('updated_at', '=', $year)
                 ->where('document_state', 'En cours')
                 ->where('status_payment', 1)
+                ->where('status_payment', 2)
                 ->get();
             //$total_current_acompte_count = count($acompte);
            // $total_current_acompte_amount = 0;
