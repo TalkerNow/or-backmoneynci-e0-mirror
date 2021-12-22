ea16630f0abde3a74a0785ce29a7bca4db585363
enzo <enzo.nivert@epitech.eu>
2021-12-22 11:45:01 +0100

    test

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -171,7 +171,6 @@ class StatisticsController extends Controller
         for($x = 1; $x < 13; $x++) {
             $monthArray[$x] = $monthData;
           }
-          return json_encode($monthArray);
             // ?  acompte em cours
             $acompte = DB::table('documents')
                 ->whereYear('updated_at', '=', $year)
