71f9b50ae580fdf9c797ee3c78f4ec803cf618dd
enzo <enzo.nivert@epitech.eu>
2021-12-28 14:53:51 +0100

    test

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -232,7 +232,8 @@ class StatisticsController extends Controller
     }
     public function getPrestation(Request $request) 
     {
-        $year = isset($request->year) ? $request->year : (int) date('Y'); // TODO remplacer valeur en dure
+        $year = isset($request->year) ? $request->year : (int) date('Y');
+        return json_encode($year);
         $monthData = array (
             'CH' => 0,
             'SIMU' => 0,
