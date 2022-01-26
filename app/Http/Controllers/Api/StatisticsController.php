c79041e4523dfb9b204fbfdbcee58cf6102ad019
enzo <enzo.nivert@epitech.eu>
2022-01-26 16:13:57 +0100

    test

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -259,7 +259,7 @@ class StatisticsController extends Controller
                 continue;
             $creator = $this->getKeyByID($memberList,  $item->creator_id);
             if ($creator ==! null && $item->document_state === 'En attente') {
-                $memberList[0]['monthArray'][(int)date('n', strtotime($item->created_at))]['creer En attente'] += 1;
+                $memberList[$creator]['monthArray'][(int)date('n', strtotime($item->created_at))]['creer En attente'] += 1;
             }
         }
         // ? member info for running contracts
