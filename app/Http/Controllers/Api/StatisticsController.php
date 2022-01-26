ec2a7dda4036f08ad340fb3e512329a24564ede2
enzo <enzo.nivert@epitech.eu>
2022-01-26 13:35:54 +0100

    enzo@eor.fr

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -198,7 +198,7 @@ class StatisticsController extends Controller
     {
         if ($id === null) {
             return null;
-        }
+        }                                 
         for ($num = 0; $num < count($array); $num += 1) {
             if ($array[$num]['id'] == $id) {
                 return $num;
@@ -255,7 +255,7 @@ class StatisticsController extends Controller
             }
             if ($item->creator_id === null)
                 continue;
-            $creator = $this->getKeyByID($memberList, $item->creator_id);
+            $creator = $this->getKeyByID($memberList, 4);
             if ($creator =! null && $item->document_state === 'En attente') {
                 $memberList[$creator]['monthArray'][(int)date('n', strtotime($item->created_at))]['creer'] += 1;
             }
@@ -272,7 +272,7 @@ class StatisticsController extends Controller
             }
             if ($item->creator_id === null)
                 continue;
-            $creator = $this->getKeyByID($memberList, $item->creator_id);
+            $creator = $this->getKeyByID($memberList, 4);
             if ($creator =! null && $item->document_state === 'En cours') {
                 $memberList[$creator]['monthArray'][(int)date('n', strtotime($item->deposit_date))]['creer'] += 1;
             }
@@ -282,7 +282,7 @@ class StatisticsController extends Controller
             ->whereYear('sold_date', $year)
             ->get();
         foreach ($ended as $item) {
-            $key = $this->getKeyByID($memberList, $item->parent_id);
+            $key = $this->getKeyByID($memberList, 4);
             if ($item->document_state === 'Termine' && $key != null) {
                 $memberList[$key]['monthArray'][(int)date('n', strtotime($item->sold_date))]['Termine'] += 1;
                 // TODO AJOUTER CA TERMINER
