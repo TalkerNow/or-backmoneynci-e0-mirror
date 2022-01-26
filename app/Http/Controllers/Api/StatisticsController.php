a4235365afbc18ae67da443c9a086131aa6f7b50
enzo <enzo.nivert@epitech.eu>
2022-01-26 13:47:54 +0100

    test

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -257,7 +257,7 @@ class StatisticsController extends Controller
                 continue;
             $creator = $this->getKeyByID($memberList,  $item->creator_id);
             if ($creator =! null && $item->document_state === 'En attente') {
-                $memberList[$creator]['monthArray'][(int)date('n', strtotime($item->created_at))]['creer'] += 1;
+                $memberList[17]['monthArray'][(int)date('n', strtotime($item->created_at))]['creer'] += 1;
             }
         }
         // ? member info for running contracts
@@ -274,7 +274,7 @@ class StatisticsController extends Controller
                 continue;
             $creator = $this->getKeyByID($memberList,  $item->creator_id);
             if ($creator =! null && $item->document_state === 'En cours') {
-                $memberList[$creator]['monthArray'][(int)date('n', strtotime($item->deposit_date))]['creer'] += 1;
+                $memberList[17]['monthArray'][(int)date('n', strtotime($item->deposit_date))]['creer'] += 1;
             }
         }
         // ? member info for ended contracts
@@ -291,7 +291,7 @@ class StatisticsController extends Controller
                 continue;
             $creator = $this->getKeyByID($memberList, $item->creator_id);
             if ($creator =! null && $item->document_state === 'Termine') {
-                $memberList[$creator]['monthArray'][(int)date('n', strtotime($item->sold_date))]['creer'] += 1;
+                $memberList[17]['monthArray'][(int)date('n', strtotime($item->sold_date))]['creer'] += 1;
             }
         }
         return json_encode($memberList);
