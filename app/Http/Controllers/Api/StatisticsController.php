a9857a61e4445b1c875644472a318effde6f7ac7
enzo <enzo.nivert@epitech.eu>
2022-02-01 12:24:43 +0100

    updating prestation in statistics controller

--- app/Http/Controllers/Api/StatisticsController.php
+++ app/Http/Controllers/Api/StatisticsController.php
@@ -144,31 +144,31 @@ class StatisticsController extends Controller
             }
         }
         $running = DB::table('documents')
-            ->whereYear('created_at', '=', $year)
+            ->whereYear('deposit_date', '=', $year)
             ->where('document_state', 'En cours')
             ->get();
         foreach ($running as $item) {
             if (strpos($item->subscribe_services, 'CH') !== false) {
-                $monthArray[1][(int)date('n', strtotime($item->updated_at))]['CH'] += 1;
+                $monthArray[1][(int)date('n', strtotime($item->deposit_date))]['CH'] += 1;
             }
             if (strpos($item->subscribe_services, ' SIMU') !== false) {
-                $monthArray[1][(int)date('n', strtotime($item->updated_at))]['SIMU'] += 1;
+                $monthArray[1][(int)date('n', strtotime($item->deposit_date))]['SIMU'] += 1;
             }
             if (strpos($item->subscribe_services, 'AR') !== false) {
-                $monthArray[1][(int)date('n', strtotime($item->updated_at))]['AR'] += 1;
+                $monthArray[1][(int)date('n', strtotime($item->deposit_date))]['AR'] += 1;
             }
             if (strpos($item->subscribe_services, 'TFD') !== false) {
-                $monthArray[1][(int)date('n', strtotime($item->updated_at))]['TFD'] += 1;
+                $monthArray[1][(int)date('n', strtotime($item->deposit_date))]['TFD'] += 1;
             }
             if (strpos($item->subscribe_services, 'ACTU') !== false) {
-                $monthArray[1][(int)date('n', strtotime($item->updated_at))]['ACTU'] += 1;
+                $monthArray[1][(int)date('n', strtotime($item->deposit_date))]['ACTU'] += 1;
             }
             if (strpos($item->subscribe_services, 'RAC') !== false) {
-                $monthArray[1][(int)date('n', strtotime($item->updated_at))]['RAC'] += 1;
+                $monthArray[1][(int)date('n', strtotime($item->deposit_date))]['RAC'] += 1;
             }
         }
         $ended = DB::table('documents')
-            ->whereYear('created_at', '=', $year)
+            ->whereYear('updated_at', '=', $year)
             ->where('document_state', 'Termine')
             ->get();
         foreach ($ended as $item) {
