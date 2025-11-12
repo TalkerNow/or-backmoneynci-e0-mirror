2510f83bf3e83e0b8ca1ed2fc6a52f1b18673c02
Rémi SALEH <remi.saleh@epitech.eu>
2025-11-12 18:17:46 +0100

    [ADD]

--- app/Models/Documents.php
+++ app/Models/Documents.php
@@ -9,29 +9,34 @@ class Documents extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        // ⚠️ enlève 'id' d'ici
-        'link_to_documents','type','document_state','comment','payment_method',
-        'advanced_payment','pre_payment','end_payment','status_payment',
-        'subscribe_services','values','user_id','parent_id','deposit_date',
-        'sold_date','creator_id','acompte_dates','sold_dates',
+        'id',
+        'link_to_documents',
+        'type',
+        'document_state',
+        'comment',
+        'payment_method',
+        'advanced_payment',
+        'pre_payment',
+        'end_payment',
+        'status_payment',
+        'subscribe_services',
+        'values',
+        'user_id',
+        'parent_id',
+        'deposit_date',
+        'sold_date',
+        'creator_id',
+        'acompte_dates',
+        'sold_dates',
     ];
-
-    protected $casts = [
-        'values'         => 'array',
-        'sold_dates'     => 'array',
-        'acompte_dates'  => 'array',
-    ];
-
-    // Valeurs par défaut envoyées si rien n’est fourni
-    protected $attributes = [
-        'sold_dates'    => '[]',
-        'acompte_dates' => '[]',
-    ];
-
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo('App\Models\User', 'user_id');
     }
 }
-
