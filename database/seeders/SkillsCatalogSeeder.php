4c3316b3fd4a71c3f5f71a0f80e659c1f1322d29
dylan <dylan.ta@epitech.eu>
2026-04-09 12:37:58 +0200

    feat: make skills path configurable via SKILLS_PATH env variable

--- database/seeders/SkillsCatalogSeeder.php
+++ database/seeders/SkillsCatalogSeeder.php
@@ -13,7 +13,7 @@ class SkillsCatalogSeeder extends Seeder
      */
     private function clientFilesPath(): string
     {
-        return base_path('../client_files/01_REGLEMENTATION/skills');
+        return env('SKILLS_PATH', base_path('../client_files/01_REGLEMENTATION/skills'));
     }
 
     public function run()
