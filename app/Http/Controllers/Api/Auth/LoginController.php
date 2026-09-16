8dda4e57edf38a942e30afaabfe6ce8e9ef06113
Patrick-Lévy N'da <gnanmienlie-patrick-levy.n-da@epitech.eu>
2026-09-16 16:29:20 +0200

    fix(auth): remove plaintext credentials logging in LoginController

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Api\Controller;
 use http\Env\Response;
 use Illuminate\Http\Request;
-use Log;
 
 class LoginController extends Controller
 {
@@ -20,9 +19,7 @@ class LoginController extends Controller
     {
         $creds = $request->only(['email', 'password']);
 
-        Log::info($creds);
         if (!$token = auth()->attempt($creds)) {
-            Log::info('ISSUE');
             return Response()->json(['error' => 'Incorrect email/password'], 401);
         }
         $name = Auth()->user()->name;
